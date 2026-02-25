0c2790255a379061e93b2af356bfb6313e089b77
Arantxa13gp <arantxa13gp@gmail.com>
2026-02-25 16:19:51 +0100

    v1.3 -Octavo commit.

--- src/Personajes/Subclases/Cazador.java
+++ src/Personajes/Subclases/Cazador.java
@@ -118,147 +118,141 @@ public class Cazador extends Personaje {
         public String tipoMascota(String raza) {
             switch (raza.toLowerCase()) {
                 case "cánido":
-                    subirNivel();
-                {
-                    setNivel(getNivel() + 1);
-                    setAtq(getAtq());
-                    setPv(getPv());
-                    setArm(getArm());
-                    setVel(getVel());
-                    setRes(getRes());
-                }
-                subirAtq();
-                {
-                    int n = (int) (Math.random() * 100) + 1;
-                    if (n >= 50) {
-                        setAtq(getAtq() + 1);
-                    }
-                }
-                subirArm();
-                {
-                    int n = (int) (Math.random() * 100) + 1;
-                    if (n >= 50) {
-                        setArm(getArm() + 1);
-                    }
-                }
-                subirPv();
-                {
-                    int n = (int) (Math.random() * 100) + 1;
-                    if (n >= 50) {
-                        setPv(getPv() + 1);
-                    }
-                }
-                subirRes();
-                {
-                    int n = (int) (Math.random() * 100) + 1;
-                    if (n >= 50) {
-                        setRes(getRes() + 1);
-                    }
-                }
-                subirVel();
-                {
-                    int n = (int) (Math.random() * 100) + 1;
-                    if (n >= 30) {
-                        setVel(getVel() + 1);
-                    }
-                }
+                    subirCánino();
                 break;
                 case "felino":
-                    subirNivel();
-                {
-                    setNivel(getNivel() + 1);
-                    setAtq(getAtq());
-                    setPv(getPv());
-                    setArm(getArm());
-                    setVel(getVel());
-                    setRes(getRes());
-                }
-                subirAtq();
-                {
-                    int n = (int) (Math.random() * 100) + 1;
-                    if (n >= 70) {
-                        setAtq(getAtq() + 1);
-                    }
-                }
-                subirArm();
-                {
-                    int n = (int) (Math.random() * 100) + 1;
-                    if (n >= 85) {
-                        setArm(getArm() + 1);
-                    }
-                }
-                subirPv();
-                {
-                    int n = (int) (Math.random() * 100) + 1;
-                    if (n >= 85) {
-                        setPv(getPv() + 1);
-                    }
-                }
-                subirRes();
-                {
-                    int n = (int) (Math.random() * 100) + 1;
-                    if (n >= 85) {
-                        setRes(getRes() + 1);
-                    }
-                }
-                subirVel();
-                {
-                    int n = (int) (Math.random() * 100) + 1;
-                    if (n >= 70) {
-                        setVel(getVel() + 1);
-                    }
-                }
+                    subirFelino();
                 break;
                 case "rapaz":
-                    subirNivel();
-                {
-                    setNivel(getNivel() + 1);
-                    setAtq(getAtq());
-                    setPv(getPv());
-                    setArm(getArm());
-                    setVel(getVel());
-                    setRes(getRes());
-                }
-                subirAtq();
-                {
-                    int n = (int) (Math.random() * 100) + 1;
-                    if (n >= 85) {
-                        setAtq(getAtq() + 1);
-                    }
-                }
-                subirArm();
-                {
-                    int n = (int) (Math.random() * 100) + 1;
-                    if (n >= 95) {
-                        setArm(getArm() + 1);
-                    }
-                }
-                subirPv();
-                {
-                    int n = (int) (Math.random() * 100) + 1;
-                    if (n >= 95) {
-                        setPv(getPv() + 1);
-                    }
-                }
-                subirRes();
-                {
-                    int n = (int) (Math.random() * 100) + 1;
-                    if (n >= 75) {
-                        setRes(getRes() + 1);
-                    }
-                }
-                subirVel();
-                {
-                    int n = (int) (Math.random() * 100) + 1;
-                    if (n >= 65) {
-                        setVel(getVel() + 1);
-                    }
-                }
+                    subirRapaz();
                 break;
                 default:
                     raza = "";
             }
             return raza;
+        }
+    }
+
+    public void subirCánino(){
+        subirNivel(); {
+            setNivel(getNivel() + 1);
+            setAtq(getAtq());
+            setPv(getPv());
+            setArm(getArm());
+            setVel(getVel());
+            setRes(getRes());
+        }
+        subirAtq(); {
+            int n = (int) (Math.random() * 100) + 1;
+            if (n >= 50) {
+                setAtq(getAtq() + 1);
+            }
+        }
+        subirArm(); {
+            int n = (int) (Math.random() * 100) + 1;
+            if (n >= 50) {
+                setArm(getArm() + 1);
+            }
+        }
+        subirPv(); {
+            int n = (int) (Math.random() * 100) + 1;
+            if (n >= 50) {
+                setPv(getPv() + 1);
+            }
+        }
+        subirRes(); {
+            int n = (int) (Math.random() * 100) + 1;
+            if (n >= 50) {
+                setRes(getRes() + 1);
+            }
+        }
+        subirVel(); {
+            int n = (int) (Math.random() * 100) + 1;
+            if (n >= 30) {
+                setVel(getVel() + 1);
+            }
+        }
+    }
+
+    public void subirFelino(){
+        subirNivel(); {
+            setNivel(getNivel() + 1);
+            setAtq(getAtq());
+            setPv(getPv());
+            setArm(getArm());
+            setVel(getVel());
+            setRes(getRes());
+        }
+        subirAtq(); {
+            int n = (int) (Math.random() * 100) + 1;
+            if (n >= 70) {
+                setAtq(getAtq() + 1);
+            }
+        }
+        subirArm(); {
+            int n = (int) (Math.random() * 100) + 1;
+            if (n >= 85) {
+                setArm(getArm() + 1);
+            }
+        }
+        subirPv(); {
+            int n = (int) (Math.random() * 100) + 1;
+            if (n >= 85) {
+                setPv(getPv() + 1);
+            }
+        }
+        subirRes(); {
+            int n = (int) (Math.random() * 100) + 1;
+            if (n >= 85) {
+                setRes(getRes() + 1);
+            }
+        }
+        subirVel(); {
+            int n = (int) (Math.random() * 100) + 1;
+            if (n >= 70) {
+                setVel(getVel() + 1);
+            }
+        }
+    }
+
+    public void subirRapaz(){
+        subirNivel(); {
+            setNivel(getNivel() + 1);
+            setAtq(getAtq());
+            setPv(getPv());
+            setArm(getArm());
+            setVel(getVel());
+            setRes(getRes());
+        }
+        subirAtq(); {
+            int n = (int) (Math.random() * 100) + 1;
+            if (n >= 85) {
+                setAtq(getAtq() + 1);
+            }
+        }
+        subirArm(); {
+            int n = (int) (Math.random() * 100) + 1;
+            if (n >= 95) {
+                setArm(getArm() + 1);
+            }
+        }
+        subirPv(); {
+            int n = (int) (Math.random() * 100) + 1;
+            if (n >= 95) {
+                setPv(getPv() + 1);
+            }
+        }
+        subirRes(); {
+            int n = (int) (Math.random() * 100) + 1;
+            if (n >= 75) {
+                setRes(getRes() + 1);
+            }
+        }
+        subirVel(); {
+            int n = (int) (Math.random() * 100) + 1;
+            if (n >= 65) {
+                setVel(getVel() + 1);
+            }
         }
     }
 
